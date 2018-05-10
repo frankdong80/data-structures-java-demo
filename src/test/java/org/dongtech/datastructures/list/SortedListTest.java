0041d9383a9fbc269b808320631ce56a6898dc18
Frank Dong <frankdong80@gmail.com>
2018-05-10 22:28:17 +0800

    commit

---
+++ src/test/java/org/dongtech/datastructures/list/SortedListTest.java
@@ -0,0 +1,56 @@
+package org.dongtech.datastructures.list;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Fuqiang
+ * Created on 2018/5/10.
+ */
+class SortedListTest {
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void remove() {
+  }
+
+  @Test
+  void getPosition() {
+  }
+
+  @Test
+  void getEntry() {
+  }
+
+  @Test
+  void contains() {
+  }
+
+  @Test
+  void remove1() {
+  }
+
+  @Test
+  void clear() {
+  }
+
+  @Test
+  void getLength() {
+  }
+
+  @Test
+  void isEmpty() {
+  }
+
+  @Test
+  void isFull() {
+  }
+
+  @Test
+  void display() {
+  }
+}
